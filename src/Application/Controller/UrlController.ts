de3909963d559e324320daa6015957ca0b840c3e
Can <yamahasirossi@gmail.com>
2022-05-14 15:05:58 +0300

    controller constructor refarance changed

--- src/Application/Controller/UrlController.ts
+++ src/Application/Controller/UrlController.ts
@@ -2,22 +2,24 @@ import { Request, Response } from 'express';
 import shortid from 'shortid';
 import UrlCreationCommand from '../../Core/Service/Command/RequestModel/UrlCreationCommand';
 import UrlCodeQuery from '../../Core/Service/Query/RequestModel/UrlCodeQuery';
-import BaseDispatcherContext from '../../Core/Service/BaseDispatcherContext';
 import FindAllUrlQuery from '../../Core/Service/Query/RequestModel/FindAllQuery';
 import ApiResponse from '../../Core/Service/ResponseModel/ApiResponse';
 import RedirectionToOriginalUrlQuery from '../../Core/Service/Query/RequestModel/RedirectionToOriginalUrlQuery';
 import RedirectLongUrlResponse from '../../Core/Service/ResponseModel/RedirectLongUrlResponse';
+import DispatcherContext from '../../Core/Service/DispatcherContext';
+import QueryRequestStrategy from '../../Core/Service/DispatcherStrategy/QueryRequestStrategy';
+import CommandRequestStrategy from '../../Core/Service/DispatcherStrategy/CommandRequestStrategy';
 
 export default class UrlController{
-    private dispatcherContext: BaseDispatcherContext
+    private dispatcherContext: DispatcherContext
     
-    public constructor(dispatcherContext: BaseDispatcherContext ){
+    public constructor(dispatcherContext: DispatcherContext ){
         this.dispatcherContext = dispatcherContext
     }
 
     public async findAll(req:Request, res:Response){
         let query = new FindAllUrlQuery()
-        let  response = <ApiResponse> await this.dispatcherContext.sendQuery(query)
+        let  response = <ApiResponse> await this.dispatcherContext.sendToDispatcher(QueryRequestStrategy.name ,query)
         
         res
             .status(response.getStatusCode())
@@ -33,7 +35,7 @@ export default class UrlController{
         creationalCommand.setUrlCode(shortid.generate())
         creationalCommand.setShortUrl(req.protocol + "://" + req.headers.host + "/api/v1/" + creationalCommand.getUrlCode() )
         
-        let response  = <ApiResponse> await this.dispatcherContext.sendCommand(creationalCommand)
+        let response  = <ApiResponse> await this.dispatcherContext.sendToDispatcher(CommandRequestStrategy.name ,creationalCommand)
         res
             .status(response.getStatusCode())
             .send(response.getResult())
@@ -46,7 +48,7 @@ export default class UrlController{
         const urlCodeQuery = new UrlCodeQuery()
         urlCodeQuery.setUrlCode(url_code)
         
-        let response = <ApiResponse> await this.dispatcherContext.sendQuery(urlCodeQuery)
+        let response = <ApiResponse> await this.dispatcherContext.sendToDispatcher(QueryRequestStrategy.name, urlCodeQuery)
         
         res
             .status(response.getStatusCode())
@@ -60,7 +62,7 @@ export default class UrlController{
         const query = new RedirectionToOriginalUrlQuery()
         query.setUrlCode(url_code)
 
-        let response = <ApiResponse> await this.dispatcherContext.sendQuery(query)
+        let response = <ApiResponse> await this.dispatcherContext.sendToDispatcher(QueryRequestStrategy.name ,query)
         
         let result = <RedirectLongUrlResponse> response.getResult()
         if(result instanceof RedirectLongUrlResponse){

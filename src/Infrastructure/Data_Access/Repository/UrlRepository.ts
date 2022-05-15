98782aae98184bf4e7e345fec9079d3726f0f8f8
Can <yamahasirossi@gmail.com>
2022-05-16 00:22:20 +0300

    concrete Url instance changed with Url Factory

--- src/Infrastructure/Data_Access/Repository/UrlRepository.ts
+++ src/Infrastructure/Data_Access/Repository/UrlRepository.ts
@@ -1,9 +1,15 @@
 import Url from "../../../Core/Domain/Url";
 import UrlDoesntExistException from "../../../Core/Exception/UrlDoesntExistException";
+import IFactory from "../../../Core/Factory/IFactory";
 import IUrlRepository from "../../../Core/Interface/IUrlRepository";
 import ORM_Url from '../../Entity/ORM_Url';
 
 export default class UrlRepository implements IUrlRepository{
+
+    private urlFactory: IFactory<Url>
+    constructor(urlFactory: IFactory<Url>){
+        this.urlFactory = urlFactory
+    }
     async findAll(): Promise<Url[]> {
         let allUrl  = await ORM_Url.find({
             order: {
@@ -11,7 +17,7 @@ export default class UrlRepository implements IUrlRepository{
             }
         })
 
-        let urlDomainObjects = allUrl.map(url => new Url(url.id, url.longUrl, url.urlCode, url.shortUrl, url.howMuchTimeClicked, url.created_at))
+        let urlDomainObjects = allUrl.map(url => this.urlFactory.createInstance(url.id, url.longUrl, url.urlCode, url.shortUrl, url.howMuchTimeClicked, url.created_at))
         return urlDomainObjects
     }
     async saveAndChanges(url: Url): Promise<void> {
@@ -21,9 +27,12 @@ export default class UrlRepository implements IUrlRepository{
     async findByUrlCode(urlCode: string): Promise<Url> {
         try {
             let orm_Url = await ORM_Url.findOne({where:{urlCode: urlCode}})
-            return new Url(orm_Url.id, orm_Url.longUrl, orm_Url.urlCode, orm_Url.shortUrl, orm_Url.howMuchTimeClicked ,orm_Url.created_at)
+
+            return this.urlFactory.createInstance(orm_Url.id, orm_Url.longUrl, orm_Url.urlCode, orm_Url.shortUrl, orm_Url.howMuchTimeClicked ,orm_Url.created_at)
         } catch (error) {
             throw new UrlDoesntExistException()
         }
     }
+
+    
 }

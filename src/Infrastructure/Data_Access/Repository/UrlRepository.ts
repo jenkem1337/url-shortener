c78b6a6ae43c4a756762670583949d897ee74f7e
Can <yamahasirossi@gmail.com>
2022-05-16 03:47:09 +0300

    entity manager added and transaction managment added

--- src/Infrastructure/Data_Access/Repository/UrlRepository.ts
+++ src/Infrastructure/Data_Access/Repository/UrlRepository.ts
@@ -3,15 +3,19 @@ import UrlDoesntExistException from "../../../Core/Exception/UrlDoesntExistExcep
 import IFactory from "../../../Core/Factory/IFactory";
 import IUrlRepository from "../../../Core/Interface/IUrlRepository";
 import ORM_Url from '../../Entity/ORM_Url';
+import { DataSource } from 'typeorm';
 
 export default class UrlRepository implements IUrlRepository{
 
     private urlFactory: IFactory<Url>
-    constructor(urlFactory: IFactory<Url>){
+    private entityManager: DataSource
+
+    constructor(urlFactory: IFactory<Url>, entityManager: DataSource){
         this.urlFactory = urlFactory
+        this.entityManager = entityManager
     }
     async findAll(): Promise<Url[]> {
-        let allUrl  = await ORM_Url.find({
+        let allUrl  = await this.entityManager.manager.find(ORM_Url,{
             order: {
                 created_at: "DESC"
             }
@@ -22,11 +26,14 @@ export default class UrlRepository implements IUrlRepository{
     }
     async saveAndChanges(url: Url): Promise<void> {
         let orm_Url = new ORM_Url(url.getUuid(), url.getLongUrl(), url.getUrlCode(),url.getShortUrl(), url.getHowMuchTimeClicked(), url.getWhenCreated())
-        await orm_Url.save()
+        
+        await this.entityManager.manager.transaction(async transactionalEntityManager => {
+            await transactionalEntityManager.save(orm_Url)
+        })
     }
     async findByUrlCode(urlCode: string): Promise<Url> {
         try {
-            let orm_Url = await ORM_Url.findOne({where:{urlCode: urlCode}})
+            let orm_Url = await this.entityManager.manager.findOne(ORM_Url,{where:{urlCode: urlCode}})
 
             return this.urlFactory.createInstance(orm_Url.id, orm_Url.longUrl, orm_Url.urlCode, orm_Url.shortUrl, orm_Url.howMuchTimeClicked ,orm_Url.created_at)
         } catch (error) {

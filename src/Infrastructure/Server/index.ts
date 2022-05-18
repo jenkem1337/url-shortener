dc92bb46a2f32e369eed1be6d3d030e1a7a5d49c
Can <yamahasirossi@gmail.com>
2022-05-18 20:46:34 +0300

    error handler middleware added

--- src/Infrastructure/Server/index.ts
+++ src/Infrastructure/Server/index.ts
@@ -1,12 +1,21 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import 'reflect-metadata'
 import cors from "cors";
 import { urlRouter } from "../../Application/Router/UrlRouter";
 import DatabaseConnection from '../Data_Access/DataResource/DatabaseConnection';
+import BaseException from '../../Core/Exception/BaseException';
+import { NextFunction } from 'express';
+import FactoryCreator from "../../Application/Factory/FactoryCreator";
+import ErrorHandlingMiddlewareFactory from '../../Application/Factory/MiddlewareFactory/ErrorHandlingFactory';
 
 const app = express()
 
-const server = async () => {
+let errorHandlingMiddleware = FactoryCreator.initFactory(new ErrorHandlingMiddlewareFactory)
+
+let handleError = (err: BaseException, req:Request, res:Response, next: NextFunction) => errorHandlingMiddleware.handleError(err, req,res,next)
+
+
+const server = async () =>{
 
 
     try {
@@ -15,11 +24,11 @@ const server = async () => {
         app.use(cors())
         app.use(express.json())
         app.use("/api/v1/", urlRouter)
+        app.use(handleError)
         app.listen(8000, () => console.log('sunucu 8000 portunda çalışıyor'))
 
     } catch (error) {
         throw new Error(error)
     }
 }
-
 server()

e006766945c4a9fbc47bb72d33d09dfec7fcfcd5
Can <yamahasirossi@gmail.com>
2022-05-18 20:49:47 +0300

    incorrect metod name changed

--- src/Core/Interface/IUrlRepository.ts
+++ src/Core/Interface/IUrlRepository.ts
@@ -1,6 +1,6 @@
 import Url from '../Domain/Url';
 export default interface IUrlRepository{
     findAll():Promise<Url[]>
-    saveAndChanges(url:Url):Promise<void>
+    saveChanges(url:Url):Promise<void>
     findByUrlCode(urlCode:string):Promise<Url>
 }

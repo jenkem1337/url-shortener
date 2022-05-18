ef08c384a2636dedc443f4d1b818459392e9c487
Can <yamahasirossi@gmail.com>
2022-05-18 20:44:42 +0300

    handling exception with try catch

--- src/Application/Controller/UrlController.ts
+++ src/Application/Controller/UrlController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import shortid from 'shortid';
 import UrlCreationCommand from '../../Core/Service/Command/RequestModel/UrlCreationCommand';
 import UrlCodeQuery from '../../Core/Service/Query/RequestModel/UrlCodeQuery';
@@ -7,6 +7,10 @@ import ApiResponse from '../../Core/Service/ResponseModel/ApiResponse';
 import RedirectionToOriginalUrlQuery from '../../Core/Service/Query/RequestModel/RedirectionToOriginalUrlQuery';
 import RedirectLongUrlResponse from '../../Core/Service/ResponseModel/RedirectLongUrlResponse';
 import DispatcherContext from '../../Core/Service/DispatcherStrategy/DispatcherContext';
+import HttpStatusCode from '../../Core/Service/ResponseModel/HttpStatus';
+import IncrementUrlCounterCommand from '../../Core/Service/Command/RequestModel/IncrementUrlCounterCommand';
+import SaveUrlCacheCommand from '../../Core/Service/Command/RequestModel/SaveUrlCacheCommand';
+
 
 export default class UrlController{
     private dispatcherContext: DispatcherContext
@@ -25,51 +29,79 @@ export default class UrlController{
         
     }
 
-    public async saveUrl(req:Request,res:Response){
-        const {longUrl} = req.body
+    public saveUrl(req:Request,res:Response, next:NextFunction){
+        try {
+            const {longUrl} = req.body
         
-        const creationalCommand = new UrlCreationCommand()
-        creationalCommand.setLongUrl(longUrl)
-        creationalCommand.setUrlCode(shortid.generate())
-        creationalCommand.setShortUrl(req.protocol + "://" + req.headers.host + "/api/v1/" + creationalCommand.getUrlCode() )
-        
-        let response  = <ApiResponse> await this.dispatcherContext.sendToDispatcher(creationalCommand)
-        res
-            .status(response.getStatusCode())
-            .send(response.getResult())
+            const creationalCommand = new UrlCreationCommand()
+            creationalCommand.setLongUrl(longUrl)
+            creationalCommand.setUrlCode(shortid.generate())
+            creationalCommand.setHowManyTimeClicked(0)
+            creationalCommand.setShortUrl(req.protocol + "://" + req.headers.host + "/api/v1/" + creationalCommand.getUrlCode() )
+            
+            this.dispatcherContext.sendToDispatcher(creationalCommand)
+            
+            res
+                .status(HttpStatusCode.OK)
+                .send(creationalCommand)
+
+        } catch (error) {
+            next(error)
+        }
+
 
     }
      
-    public async findOneByUrlCode(req:Request,res:Response){
-        const {url_code} = req.params
+    public async findOneByUrlCode(req:Request,res:Response, next:NextFunction){
+        try {
+            const {url_code} = req.params
         
-        const urlCodeQuery = new UrlCodeQuery()
-        urlCodeQuery.setUrlCode(url_code)
-        
-        let response = <ApiResponse> await this.dispatcherContext.sendToDispatcher(urlCodeQuery)
-        
-        res
-            .status(response.getStatusCode())
-            .send(response.getResult())
+            const urlCodeQuery = new UrlCodeQuery()
+            urlCodeQuery.setUrlCode(url_code)
+            
+            let saveCacheCommand = new SaveUrlCacheCommand()
+            saveCacheCommand.setUrlCode(url_code)
+            
+            this.dispatcherContext.sendToDispatcher(saveCacheCommand)
 
-    }
+            let response = <ApiResponse> await this.dispatcherContext.sendToDispatcher(urlCodeQuery)
+            
+            res
+                .status(response.getStatusCode())
+                .send(response.getResult())
+    
 
-    public async redirectToOriginalUrl(req:Request,res:Response){
-        const {url_code} = req.params
-
-        const query = new RedirectionToOriginalUrlQuery()
-        query.setUrlCode(url_code)
-
-        let response = <ApiResponse> await this.dispatcherContext.sendToDispatcher(query)
-        
-        let result = <RedirectLongUrlResponse> response.getResult()
-        if(result instanceof RedirectLongUrlResponse){
-            res.redirect(result.getLongUrl())
+        } catch (error) {
+            next(error)
         }
-        res.status(response.getStatusCode()).send(response.getResult())
-        
-        
-        
-
     }
+
+    public async redirectToOriginalUrl(req:Request,res:Response, next:NextFunction){
+            try {
+                const {url_code} = req.params
+                
+                let incerementCounterCommand = new IncrementUrlCounterCommand()
+                incerementCounterCommand.setUrlCode(url_code)
+    
+    
+                const query = new RedirectionToOriginalUrlQuery()
+                query.setUrlCode(url_code)
+    
+                this.dispatcherContext.sendToDispatcher(incerementCounterCommand)
+                let response = <ApiResponse> await this.dispatcherContext.sendToDispatcher(query)
+
+
+                let result = <RedirectLongUrlResponse> response.getResult()
+                if(result instanceof RedirectLongUrlResponse){
+                    res.redirect(result.getLongUrl())
+                }
+            } catch (error) {
+                next(error)
+            }
+        
+    }
+
+
+
+    
 }

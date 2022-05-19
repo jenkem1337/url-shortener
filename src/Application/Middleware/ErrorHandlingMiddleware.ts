3584fe8dd61e6929f41c713c64cd55e79e52093d
Can <yamahasirossi@gmail.com>
2022-05-19 11:36:42 +0300

    commit

--- src/Application/Middleware/ErrorHandlingMiddleware.ts
+++ src/Application/Middleware/ErrorHandlingMiddleware.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express"
 import BaseException from "../../Core/Exception/BaseException"
 import ApiResponse from "../../Core/Service/ResponseModel/ApiResponse"
 import ErrorResponse from "../../Core/Service/ResponseModel/ErrorResponse"
+import HttpStatusCode from '../../Core/Service/ResponseModel/HttpStatus';
 
 export default class ErrorHandlingMiddleware {
     
@@ -9,7 +10,7 @@ export default class ErrorHandlingMiddleware {
         let response: ApiResponse
         if(!(err instanceof BaseException)){
             let e = <Error> err
-             response = new ApiResponse(404, new ErrorResponse(e.message))
+             response = new ApiResponse(HttpStatusCode.BAD_REQUEST, new ErrorResponse(e.message))
              res.status(response.getStatusCode()).send(response.getResult())
         }
         response = new ApiResponse(err.statusCode, new ErrorResponse(err.message))

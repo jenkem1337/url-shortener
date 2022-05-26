747d7d012245314f96f7548d5841e13f9e5df0d6
Can <yamahasirossi@gmail.com>
2022-05-26 13:25:26 +0300

    asyncHandler added

--- src/Application/Router/UrlRouter.ts
+++ src/Application/Router/UrlRouter.ts
@@ -11,9 +11,13 @@ const redisMiddleware =  FactoryCreator.initFactory(new RedisMiddlewareFactory)
 //Middleware
 const urlCodeCache = (req:Request,res:Response,next:NextFunction) => redisMiddleware.getUrl(req, res, next)
 
+const asyncHandler = (fn:any) => (req:Request,res:Response, next:NextFunction) => {
+    Promise.resolve(fn(req,res,next)).catch(error => next(error))
+}                                  
+
 
 urlRouter.get("/",                                        (req:Request,res:Response) =>  urlController.findAll(req, res))
-urlRouter.get("/:url_code",                               (req:Request,res:Response, next:NextFunction) =>  urlController.redirectToOriginalUrl(req,res, next))
-urlRouter.post("/save-url" ,                              (req:Request,res:Response, next:NextFunction) =>  urlController.saveUrl(req,res,next))
-urlRouter.get("/find-by-urlcode/:url_code", urlCodeCache, (req:Request,res:Response, next:NextFunction) =>  urlController.findOneByUrlCode(req,res, next))
+urlRouter.get("/:url_code",                               asyncHandler((req:Request,res:Response, next:NextFunction) =>  urlController.redirectToOriginalUrl(req,res, next)))
+urlRouter.post("/save-url" ,                              asyncHandler((req:Request,res:Response, next:NextFunction) =>  urlController.saveUrl(req,res,next)))
+urlRouter.get("/find-by-urlcode/:url_code", urlCodeCache, asyncHandler((req:Request,res:Response, next:NextFunction) =>  urlController.findOneByUrlCode(req,res,next)))
 export {urlRouter}

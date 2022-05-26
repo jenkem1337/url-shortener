c1b426330f194a040c19b88d1a1677049f6bdd8b
Can <yamahasirossi@gmail.com>
2022-05-26 13:23:23 +0300

    ApiResponse object returning

--- src/Core/Service/Query/Handler/FindCachedUrlByUrlCodeQueryHandler.ts
+++ src/Core/Service/Query/Handler/FindCachedUrlByUrlCodeQueryHandler.ts
@@ -1,18 +1,20 @@
 import IQueryHandler from '../IQueryHandler';
 import FindCachedUrlByUrlCodeQuery from '../RequestModel/FindCachedUrlByUrlCodeQuery';
 import ICacheRepository from '../../../Interface/ICacheRepository';
-export default class FindCachedUrlByUrlCodeQueryHandler implements IQueryHandler<FindCachedUrlByUrlCodeQuery, Promise<object>> {
+import ApiResponse from '../../ResponseModel/ApiResponse';
+import HttpStatusCode from '../../ResponseModel/HttpStatus';
+import CachedUrlResponse from '../../ResponseModel/CachedUrlResponse';
+export default class FindCachedUrlByUrlCodeQueryHandler implements IQueryHandler<FindCachedUrlByUrlCodeQuery, Promise<ApiResponse>> {
     
     private redisRepository: ICacheRepository
 
     constructor(redisRepo: ICacheRepository){
         this.redisRepository = redisRepo
     }
-    async execute(query: FindCachedUrlByUrlCodeQuery): Promise<object> {
-        const url = await this.redisRepository.getUrl(query.getUrlCode())
-           
+    async execute(query: FindCachedUrlByUrlCodeQuery): Promise<ApiResponse> {
+        let url = await this.redisRepository.getUrl(query.getUrlCode())
         if(Object.keys(url).length !== 0){
-            return url
+            return new ApiResponse(HttpStatusCode.OK, new CachedUrlResponse(url.longUrl, url.shortUrl, new Number(url.howMuchTimeClicked).valueOf() ))
         }
 
     }

3ba86ae4b6d3d7177bf32cf5720ca17604c1ed98
Can <yamahasirossi@gmail.com>
2022-05-26 13:22:33 +0300

    more logic added

--- src/Infrastructure/Data_Access/Repository/RedisRepository.ts
+++ src/Infrastructure/Data_Access/Repository/RedisRepository.ts
@@ -3,18 +3,23 @@ import ICacheRepository from '../../../Core/Interface/ICacheRepository';
 import Url from '../../../Core/Domain/Url';
 
 export default class RedisRepository implements ICacheRepository{
-    private client: any;
+    private client: Redis;
     
     public constructor(redis:Redis){
         this.client = redis
         
     }
-    public async saveUrl(url: Url) {
+    public async saveChanges(url: Url):Promise<void> {
+        let cachedUrl = await this.client.hgetall(url.getUrlCode())
+        if(cachedUrl){
+            await this.client.hmset(url.getUrlCode(), 'shortUrl', url.getShortUrl(), 'longUrl', url.getLongUrl(), 'howMuchTimeClicked', url.getHowMuchTimeClicked())
+            await this.client.expire(url.getUrlCode(), 60)    
+        }      
         await this.client.hmset(url.getUrlCode(), 'shortUrl', url.getShortUrl(), 'longUrl', url.getLongUrl(), 'howMuchTimeClicked', url.getHowMuchTimeClicked())
-        await this.client.expire(url.getUrlCode(), 10)
+        await this.client.expire(url.getUrlCode(), 60)
 
     }
-    public async getUrl(urlCode:string) {
+    public async getUrl(urlCode:string):Promise<any> {
         return await this.client.hgetall(urlCode)        
     }
 }

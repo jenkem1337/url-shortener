703ebac673705f7de79f26520f1d61aa63485659
Can <yamahasirossi@gmail.com>
2022-05-26 13:25:11 +0300

    method name changed and return type added

--- src/Core/Interface/ICacheRepository.ts
+++ src/Core/Interface/ICacheRepository.ts
@@ -1,5 +1,5 @@
 import Url from '../Domain/Url';
 export default interface ICacheRepository{
-    getUrl(urlCode:string)
-    saveUrl(urlCacheDto:Url)
+    getUrl(urlCode:string):Promise<any>
+    saveChanges(url:Url):Promise<void>
 }

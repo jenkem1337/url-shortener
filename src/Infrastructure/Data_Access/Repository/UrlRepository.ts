3d60ef12f8d1092e705963d409aa7eb82ed9d7ff
Can <yamahasirossi@gmail.com>
2024-04-10 17:21:33 +0300

    entityManager property type changed with EntityManager instead DataSource

--- src/Infrastructure/Data_Access/Repository/UrlRepository.ts
+++ src/Infrastructure/Data_Access/Repository/UrlRepository.ts
@@ -3,19 +3,19 @@ import UrlDoesntExistException from "../../../Core/Exception/UrlDoesntExistExcep
 import IFactory from "../../../Core/Factory/IFactory";
 import IUrlRepository from "../../../Core/Interface/IUrlRepository";
 import ORM_Url from '../../Entity/ORM_Url';
-import { DataSource } from 'typeorm';
+import { DataSource, EntityManager } from 'typeorm';
 
 export default class UrlRepository implements IUrlRepository{
 
     private urlFactory: IFactory<Url>
-    private entityManager: DataSource
+    private entityManager: EntityManager
 
-    constructor(urlFactory: IFactory<Url>, entityManager: DataSource){
+    constructor(urlFactory: IFactory<Url>, entityManager: EntityManager){
         this.urlFactory = urlFactory
         this.entityManager = entityManager
     }
     async findAll(): Promise<Url[]> {
-        let allUrl  = await this.entityManager.manager.find(ORM_Url,{
+        let allUrl  = await this.entityManager.find(ORM_Url,{
             order: {
                 created_at: "DESC"
             }
@@ -26,14 +26,12 @@ export default class UrlRepository implements IUrlRepository{
     }
     async saveChanges(url: Url): Promise<void> {
         let orm_Url = new ORM_Url(url.getUuid(), url.getLongUrl(), url.getUrlCode(),url.getShortUrl(), url.getHowMuchTimeClicked(), url.getWhenCreated())
-       
-        await this.entityManager.manager.transaction(async transactionalEntityManager => {
-            await transactionalEntityManager.save(orm_Url)
-        })
+        await this.entityManager.save(orm_Url)
+
     }
     async findByUrlCode(urlCode: string): Promise<Url> {
         try {
-            let orm_Url = await this.entityManager.manager.findOne(ORM_Url,{where:{urlCode: urlCode}})
+            let orm_Url = await this.entityManager.findOne(ORM_Url,{where:{urlCode: urlCode}})
 
             return this.urlFactory.createInstance(orm_Url.id, orm_Url.longUrl, orm_Url.urlCode, orm_Url.shortUrl, orm_Url.howMuchTimeClicked ,orm_Url.created_at)
         } catch (error) {
